d09481bf1c930f7a7da9ca26b9899b08b5d158ef
falakh <zulfikarfahmi38@gmail.com>
2018-05-07 15:22:33 +0700

    Menambahkan controller pengajuan

---
+++ application/controllers/PengajuanController.php
@@ -0,0 +1,21 @@
+<?php
+  
+  defined('BASEPATH') OR exit('No direct script access allowed');
+  
+  class PengajuanController extends CI_Controller {
+  
+      public function index()
+      {
+            $nama = $this->input->post('nama');
+            $nik = $this->input->post('nik');
+            $perihal = $this->input->post('perihal');
+            $this->load->model('User');
+            $this->User->pengajuan($nama,$nik,$perihal);
+      }
+  
+  }
+  
+  /* End of file PengejuanController.php */
+  
+
+ ?>
