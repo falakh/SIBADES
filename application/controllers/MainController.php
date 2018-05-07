0220337185e9635031cb3e6a6bd12c16801475eb
falakh <zulfikarfahmi38@gmail.com>
2018-05-07 15:21:52 +0700

    add controller to link

---
+++ application/controllers/MainController.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MainController extends CI_Controller {
+
+    public function pengajuan()
+    {
+        $this->load->library('session');
+        if($this->session->userdata('username')){
+            $this->load->view('pengajuan');
+        }else{
+             redirect('','refresh');
+        }
+    }
+    public function home(){
+        $this->load->model("User");
+        $data = array("admin" => $this->User->isAdmin());
+          $this ->load->view('home',$data); 
+    }
+    public function index()
+    {
+        $this->load->model('User');
+        $user = $this->User->getUserName();
+         if(isset($user)){
+            
+            redirect('/user/home','refresh');
+            
+        }else{
+             $this->load->view('login');
+            //redirect('','refresh');
+
+        }
+    }
+}
+
+/* End of file MainController.php */
+
+?>
