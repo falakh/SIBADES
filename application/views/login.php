ceee6dc2783e5be3960ec34ffc4f3dee3e8263fd
tifoaudi <tifofilkomub@gmail.com>
2018-05-08 23:19:16 +0700

    testing jquery failed

--- application/views/login.php
+++ application/views/login.php
@@ -5,16 +5,25 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>SIBADES - Sistem Informasi Balai Desa</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <link rel="stylesheet" type="text/css" href="../SIBADES/assets/style/login.css" />
   <link rel="stylesheet" type="text/css" href="../SIBADES/assets/semantic/semantic.min.css">
+  <script src="../SIBADES/assets/library/jquery.min.s"></script>
+  <script type="text/javascript" src="../SIBADES/assets/semantic/semantic.js"></script>
+  
+  <script language='javascript'>
+    
+      $('.ui.accordion')
+      .accordion();
+    
+  </script>
+
+  
 </head>
 
-<script>
-  
-</script>
 <body>
-  
-  <div class="ui attached secondary menu">
+  <div class="ui sticky" >
+  <div class="ui attached  secondary menu" id="sticky-header">
     <div class="ui container" id="header">
     <div class="ui image" id="header">
       <img src="../SIBADES/assets/image/logo.png" width = "460" height = "1000">
@@ -42,10 +51,48 @@
     </div>
   </div>
 
+  </div>
+  
   <div class="main-page">
     <img class="ui fluid image" src="../SIBADES/assets/image/container.png" >
   </div>
 
+  <div class="ui center aligned container">
+    <h1 class="ui header">
+      <div class="content">
+        Ajukan Surat
+        <div class="sub header"><h3>Pengajuan surat-surat kebutuhan warga desa</h3></div>
+        <div class="sub header"><button class="ui large primary button" id="pengajuan">Mulai</button></div>
+      </div>
+    </h1>
+  </div>
+
+
+  <div class="ui styled accordion">
+  <div class="active title">
+    <i class="dropdown icon"></i>
+    What is a dog?
+  </div>
+  <div class="active content">
+    <p>A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.</p>
+  </div>
+  <div class="title">
+    <i class="dropdown icon"></i>
+    What kinds of dogs are there?
+  </div>
+  <div class="content">
+    <p>There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.</p>
+  </div>
+  <div class="title">
+    <i class="dropdown icon"></i>
+    How do you acquire a dog?
+  </div>
+  <div class="content">
+    <p>Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.</p>
+    <p>A pet shop may be the most convenient way to buy a dog. Buying a dog from a private owner allows you to assess the pedigree and upbringing of your dog before choosing to take it home. Lastly, finding your dog from a shelter, helps give a good home to a dog who may not find one so readily.</p>
+  </div>
+</div>
+  
   
 </body>
 </html>
