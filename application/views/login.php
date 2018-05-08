1d9559d39a06e785af5f02f6d375733c75464afa
tifoaudi <tifofilkomub@gmail.com>
2018-05-08 13:44:46 +0700

    create homepage sibades

--- application/views/login.php
+++ application/views/login.php
@@ -1,29 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welocome To SiBADES</title>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SIBADES - Sistem Informasi Balai Desa</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../SIBADES/assets/style/login.css" />
+  <link rel="stylesheet" type="text/css" href="../SIBADES/assets/semantic/semantic.min.css">
 </head>
 
+<script>
+  
+</script>
 <body>
-    <form action= "<?php echo base_url('user/login'); 
+  
+  <div class="ui attached secondary menu">
+    <div class="ui container" id="header">
+    <div class="ui image" id="header">
+      <img src="../SIBADES/assets/image/logo.png" width = "460" height = "1000">
+    </div>
+      <a class="item">
+        Beranda
+      </a>
+      <a class="item">
+        Informasi
+      </a>
+      <a class="item">
+        Bantuan
+      </a>
+      <div class="right menu">
+        <button class="ui basic positive button">
+          Login
+        </button>
+        <button class="ui primary button">
+          Daftar
+        </button>
+      </div>
+      <div class="ui icon right dropdown">
+        <i class="large bars icon"></i>
+      </div>
+    </div>
+  </div>
 
-?>"
-    method="POST">
-        <div>
-            userName : <input type="text" name="userName">
-        </div>
-        <div>
-            password : <input type="password" name="password">
-        </div>
-        <div>
-            <input type="submit" value="send">
-        </div>
-    </form>
+  <div class="main-page">
+    <img class="ui fluid image" src="../SIBADES/assets/image/container.png" >
+  </div>
 
+  
 </body>
-
 </html>
