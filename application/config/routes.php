6447d7ea8db2b340bc121568b369dd32d33e2189
falakh <zulfikarfahmi38@gmail.com>
2018-05-10 05:26:16 +0700

    add routing untuk homepage

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'MainController';
+$route['login'] = 'MainController/login';
 $route['user/login'] = 'LoginController';
 $route['user/logout']='LogoutController';
 $route['user/pengajuan']='MainController/pengajuan';
