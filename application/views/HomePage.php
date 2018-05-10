40dc330728ac3b6de520c27e1e7dda20bd6926c3
tifoaudi <tifofilkomub@gmail.com>
2018-05-10 18:46:55 +0700

    adding login screen

--- application/views/HomePage.php
+++ application/views/HomePage.php
@@ -23,92 +23,26 @@
 </head>
 
 <body>
-    <div class="ui sticky">
-        <div class="ui attached  secondary menu" id="sticky-header">
-            <div class="ui container" id="header">
-                <div class="ui image" id="header">
-                    <img src="../SIBADES/assets/image/logo.png" width="460" height="1000">
-                </div>
-                <a class="item">
-        Beranda
-      </a>
-                <a class="item">
-        Informasi
-      </a>
-                <a class="item">
-        Bantuan
-      </a>
-      <?php
-        // mengecek user login atau tidak
-        if(isset($_POST["username"])){
-          if($_POST['admin']){
-
-          }
-        }
-
-      ?>
-                      <div class="right menu">
-                    <a href="login">
-                        <button class="ui basic positive button">
-          Login
-        </button>
-                    </a>
-                    <a href="register">
-                    <button class="ui primary button">
-          Daftar
-        </button></a>
-                </div>
-                <div class="ui icon right dropdown">
-                    <i class="large bars icon"></i>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
+  
+		<?php include ('components/HeaderBar.php'); ?>
+		
     <div class="main-page">
         <img class="ui fluid image" src="../SIBADES/assets/image/container.png">
     </div>
 
     <div class="ui center aligned container">
-        <h1 class="ui header">
-            <div class="content">
-                Ajukan Surat
-                <div class="sub header">
-                    <h3>Pengajuan surat-surat kebutuhan warga desa</h3>
-                </div>
-                <div class="sub header"><button class="ui large primary button" id="pengajuan">Mulai</button></div>
-            </div>
-        </h1>
+    	<h1 class="ui header">
+        <div class="content">
+          Ajukan Surat
+          <div class="sub header">
+            <h3>Pengajuan surat-surat kebutuhan warga desa</h3>
+          </div>
+        	<div class="sub header"><button class="ui large primary button" id="pengajuan">Mulai</button></div>
+        </div>
+      </h1>
     </div>
 
 
-    <div class="ui accordion">
-        <div class="title">
-            <i class="dropdown icon"></i> lalalala
-        </div>
-        <div class="content">
-            kakakakak
-        </div>
-        <div class="title">
-            <i class="dropdown icon"></i> lalalala
-        </div>
-        <div class="content">
-            kakakakak
-        </div>
-        <div class="title">
-            lalalala
-        </div>
-        <div class="content">
-            kakakakak
-        </div>
-        <div class="title">
-            lalalala
-        </div>
-        <div class="content">
-            kakakakak
-        </div>
-    </div>
 
 
 </body>
