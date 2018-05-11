f9aae69d504812e5e56f36e2022a051d42195768
falakh <zulfikarfahmi38@gmail.com>
2018-05-11 16:36:03 +0700

    add getAllpengajuan , accpengajuan, tolak

--- application/controllers/PengajuanController.php
+++ application/controllers/PengajuanController.php
@@ -12,7 +12,37 @@
             $this->load->model('User');
             $this->User->pengajuan($nama,$nik,$perihal);
       }
-      public function pengajuan(){
+      public function Allpengajuan()
+      {
+          $this->load->library("session");
+          $this->load->model("Admin");
+        if($this->session->admin){
+        $result =  $this->Admin->getAllPengajuan();
+          echo json_encode($result);
+        }
+      }
+      public function accPengajuan($id)
+      {
+          $this->load->library("session");
+        if($this->session->admin){
+            $this->load->model("Admin");
+            $this->Admin->setujui($id);
+       }
+      }
+         public function tolak($id,$pesan)
+      {
+          $this->load->library("session");
+         $this->session->userdata("admin"); 
+        if(isset($_SESSION["username"])){
+          if(($_SESSION['admin'])){
+            // do when user is admin
+            $this->load->model("Admin");
+            $this->Admin->tolak($id,$pesan);
+          }
+        }
+        else{
+            echo "fail";
+        }
       }
   
   }
